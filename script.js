879ac0d53efed79e2ec5e80cf3504e178d5296dd
sambhavgupta0705 <81870866+sambhavgupta0705@users.noreply.github.com>
2022-08-26 07:55:08 +0530

    Add files via upload
    Adding Files

---
+++ script.js
@@ -0,0 +1,32 @@
+const myTags = [
+    'JavaScript', 'CSS', 'HTML',
+    'C', 'C++', 'React',
+    'Python', 'Java', 'git',
+    'django', 'Node.js', 'OpenCV',
+    'GCP', 'MySQL', 'jQuery',
+];
+
+var tagCloud = TagCloud('.content', myTags,{
+
+  // radius in px
+  radius: 250,
+
+  // animation speed
+  // slow, normal, fast
+  maxSpeed: 'fast',
+  initSpeed: 'fast',
+
+  // 0 = top
+  // 90 = left
+  // 135 = right-bottom
+  direction: 135,
+  
+  // interact with cursor move on mouse out
+  keep: true
+  
+});
+
+//To change the color of text randomly
+var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0'];
+var random_color = colors[Math.floor(Math.random() * colors.length)];
+document.querySelector('.content').style.color = random_color;
